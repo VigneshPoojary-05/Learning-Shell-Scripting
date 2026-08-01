5d03827251a556bdf575f34b3484a878daeeaf7e
VigneshPoojary-05 <vigneshpoojary005@gmail.com>
2026-08-01 07:18:32 +0000

    show_executable_location

---
+++ show_executable_location.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "****************Show all executable locations*******************"
+echo "where is bash: $(whereis bash)"
+echo "where is ls: $(whereis ls)"
+echo "where is pwd: $(whereis pwd)"
+echo "where is kernel: $(whereis kernel)"
