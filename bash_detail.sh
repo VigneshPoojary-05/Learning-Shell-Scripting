fc0ec639319a95a60781057eb270309004018a02
VigneshPoojary-05 <vigneshpoojary005@gmail.com>
2026-08-01 06:46:51 +0000

    bash_detail

---
+++ bash_detail.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "****Check your current shell****"
+echo "My Current shell is: $SHELL"
+echo " "
+echo "****Check Bash version****"
+echo "Version is: $(bash --version)"
+echo " "
+echo "****Find where Bash is installed****"
+echo "Installed In $(which bash)"
